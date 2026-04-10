2c684f0d5f4e871231a82502e7233ebff08eeb36
Ansuman Nayak <anayak306@gatech.edu>
2026-04-10 17:02:31 +0000

    updated setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "=== [1/8] Installing pip dependencies ==="
-pip install --upgrade torch torchvision --break-system-packages
+pip install --upgrade torch torchvision
 pip install \
   pandas \
   numpy \
@@ -17,7 +17,11 @@ pip install \
   bitsandbytes \
   accelerate \
   py7zr \
-  --break-system-packages
+  matplotlib \
+  scipy \
+  rich \
+  ipykernel \
+  typer
 
 echo ""
 echo "=== [2/8] Creating required directories ==="
@@ -58,20 +62,21 @@ else:
     print('WARNING: Archive not found at', archive)
 "
 
-# echo ""
-# echo "=== [7/8] Downloading CNNDetection weights ==="
-# wget -q --show-progress \
-#   -O /workspace/benchmarks_ai_research/CNNDetection/checkpoints/blur_jpg_prob0.1.pth \
-#   "https://www.dropbox.com/s/h7tkpcgiwuftb6g/blur_jpg_prob0.1.pth?dl=1"
+echo ""
+echo "=== [7/8] Downloading CNNDetection weights ==="
+wget -q --show-progress \
+  -O /workspace/benchmarks_ai_research/CNNDetection/checkpoints/blur_jpg_prob0.1.pth \
+  "https://www.dropbox.com/s/h7tkpcgiwuftb6g/blur_jpg_prob0.1.pth?dl=1"
 
-# wget -q --show-progress \
-#   -O /workspace/benchmarks_ai_research/CNNDetection/checkpoints/blur_jpg_prob0.5.pth \
-#   "https://www.dropbox.com/s/2g2jagq2jn1fd0i/blur_jpg_prob0.5.pth?dl=1"
+wget -q --show-progress \
+  -O /workspace/benchmarks_ai_research/CNNDetection/checkpoints/blur_jpg_prob0.5.pth \
+  "https://www.dropbox.com/s/2g2jagq2jn1fd0i/blur_jpg_prob0.5.pth?dl=1"
 
 echo ""
 echo "=== [8/8] Verifying imports ==="
 python -c "
 import torch, torchvision, pandas, numpy, cv2, tqdm, sklearn, joblib, PIL, py7zr
+import matplotlib, scipy, rich, typer
 from decord import VideoReader, cpu
 from transformers import (
     AutoProcessor,
